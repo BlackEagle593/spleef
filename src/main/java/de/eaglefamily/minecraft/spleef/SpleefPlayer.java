822e3b594c6d1e637b74bcd41f349f0d8443967d
BlackEagle <blackeagle.eaglefamily@gmail.com>
2020-08-02 15:10:45 +0200

    Implement killstreaks

--- src/main/java/de/eaglefamily/minecraft/spleef/SpleefPlayer.java
+++ src/main/java/de/eaglefamily/minecraft/spleef/SpleefPlayer.java
@@ -1,5 +1,7 @@
 package de.eaglefamily.minecraft.spleef;
 
+import de.eaglefamily.minecraft.spleef.i18n.Translator;
+import java.util.List;
 import java.util.Objects;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeInstance;
@@ -12,24 +14,30 @@ public class SpleefPlayer {
 
   public static final String DESTROYED_METAKEY = "destroyedBy";
   public static final String SPLEEFED_METAKEY = "spleefedBy";
+  private static final List<Integer> killstreakBroadcasts = List.of(5, 10, 20, 50, 100);
 
   private final Player player;
   private final Plugin plugin;
+  private final Translator translator;
   private final SpleefItemFactory spleefItemFactory;
 
-  private SpleefPlayer(Player player, Plugin plugin, SpleefItemFactory spleefItemFactory) {
+  private SpleefPlayer(Player player, Plugin plugin, Translator translator,
+      SpleefItemFactory spleefItemFactory) {
     this.player = player;
     this.plugin = plugin;
+    this.translator = translator;
     this.spleefItemFactory = spleefItemFactory;
   }
 
-  static SpleefPlayer create(Player player, Plugin plugin, SpleefItemFactory spleefItemFactory) {
-    return new SpleefPlayer(player, plugin, spleefItemFactory);
+  static SpleefPlayer create(Player player, Plugin plugin, Translator translator,
+      SpleefItemFactory spleefItemFactory) {
+    return new SpleefPlayer(player, plugin, translator, spleefItemFactory);
   }
 
   public void resetState() {
     AttributeInstance maxHealth = player.getAttribute(Attribute.GENERIC_MAX_HEALTH);
     player.setHealth(Objects.nonNull(maxHealth) ? maxHealth.getValue() : 20);
+    player.setLevel(0);
     player.removeMetadata(SPLEEFED_METAKEY, plugin);
   }
 
@@ -41,8 +49,13 @@ public class SpleefPlayer {
   }
 
   public void addKill() {
+    int killstreak = player.getLevel() + 1;
+    player.setLevel(killstreak);
+    if (killstreakBroadcasts.contains(killstreak)) {
+      translator.broadcastMessage("killstreak", player.getName(), killstreak);
+    }
+
     // TODO - add kill to stats
-    // TODO - implement killstreak logic
   }
 
   public void addDeath() {
--- src/main/java/de/eaglefamily/minecraft/spleef/SpleefPlayerPool.java
+++ src/main/java/de/eaglefamily/minecraft/spleef/SpleefPlayerPool.java
@@ -5,6 +5,7 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import com.google.common.collect.Maps;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import de.eaglefamily.minecraft.spleef.i18n.Translator;
 import java.util.Map;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
@@ -13,12 +14,15 @@ import org.bukkit.plugin.Plugin;
 public class SpleefPlayerPool {
 
   private final Plugin plugin;
+  private final Translator translator;
   private final SpleefItemFactory spleefItemFactory;
   private final Map<Player, SpleefPlayer> spleefPlayerMap = Maps.newConcurrentMap();
 
   @Inject
-  public SpleefPlayerPool(Plugin plugin, SpleefItemFactory spleefItemFactory) {
+  public SpleefPlayerPool(Plugin plugin, Translator translator,
+      SpleefItemFactory spleefItemFactory) {
     this.plugin = plugin;
+    this.translator = translator;
     this.spleefItemFactory = spleefItemFactory;
   }
 
@@ -28,7 +32,7 @@ public class SpleefPlayerPool {
 
   public SpleefPlayer addPlayer(Player player) {
     checkNotNull(player);
-    SpleefPlayer spleefPlayer = SpleefPlayer.create(player, plugin, spleefItemFactory);
+    SpleefPlayer spleefPlayer = SpleefPlayer.create(player, plugin, translator, spleefItemFactory);
     spleefPlayerMap.put(player, spleefPlayer);
     return spleefPlayer;
   }
